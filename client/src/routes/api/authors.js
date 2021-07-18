84c1faa755292ded4a5afc4faa4f520be768ab2f
alv67 <alv67.web@gmail.com>
2021-07-18 02:44:08 +0200

    Books and Authors from fake API: linted(2)

--- client/src/routes/api/authors.js
+++ client/src/routes/api/authors.js
@@ -1,6 +1,6 @@
 import authors from '../../json/authors.json'
 
-export async function get (request) { 
+export async function get (request) {
   return {
     status: 200,
     body: authors
--- client/src/routes/api/books.js
+++ client/src/routes/api/books.js
@@ -3,6 +3,6 @@ import books from '../../json/books.json'
 export async function get (request) {
   return {
     status: 200,
-    body: books   
+    body: books
   }
 }
